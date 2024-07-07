1547fbdb77935cc4e77bf0058ed426fc5b23273b
Artsiom Dadychanka <a.dadychenko@gmail.com>
2024-07-07 17:05:19 +0500

    Setup dev tools

---
+++ .eslintrc.cjs
@@ -0,0 +1,38 @@
+module.exports = {
+  "root": true,
+  "env": {
+    "browser": true,
+    "es2021": true,
+    jest: true,
+  },
+  "extends": [
+    "next/core-web-vitals",
+    "prettier",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:jest-dom/recommended",
+    "plugin:testing-library/react",
+    "plugin:prettier/recommended"
+  ],
+  "ignorePatterns": ["dist", ".eslintrc.cjs"],
+  "parserOptions": {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: { jsx: true },
+  },
+  settings: { react: { version: "^18" } },
+  plugins: ["jest-dom", "jsx-a11y"],
+  rules: {
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
+    "prettier/prettier": [
+      "warn",
+      {
+        endOfLine: "auto",
+      },
+    ],
+    "react/jsx-no-target-blank": "off",
+  }
+}
